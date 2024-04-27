c07170abad8cc5e2b085b14c77820a13592e5a55
myInstagramAlternative <petar@jesteibice.com>
2024-04-27 17:33:24 +0200

    refactored

--- bond/sms.go
+++ bond/sms.go
@@ -2,35 +2,36 @@ package bond
 
 import (
 	"log"
-	"time"
 
 	"github.com/warthog618/modem/gsm"
 	"github.com/warthog618/sms"
 )
 
-type MessagingService struct {
-	Modem   *gsm.GSM
-	Timeout time.Duration
-}
+func (atc *ATCommander) SendSMS(number string, message string, pdu bool) error {
 
-func NewMessagingService(modem *gsm.GSM, timeout time.Duration) *MessagingService {
-	return &MessagingService{
-		Modem:   modem,
-		Timeout: timeout,
+	gopts := []gsm.Option{gsm.WithTextMode}
+	g := gsm.New(atc.AT, gopts...)
+	if err := g.Init(); err != nil {
+		return err
 	}
-}
 
-func (s *MessagingService) Send(number string, message string, pdu bool) error {
 	var err error
 	if pdu {
-		err = s.sendPDU(number, message)
+		err = atc.sendPDU(number, message)
 	} else {
-		_, err = s.Modem.SendShortMessage(number, message)
+		_, err = g.SendShortMessage(number, message)
 	}
 	return err
 }
 
-func (s *MessagingService) sendPDU(number string, message string) error {
+func (atc *ATCommander) sendPDU(number string, message string) error {
+
+	gopts := []gsm.Option{gsm.WithTextMode}
+	g := gsm.New(atc.AT, gopts...)
+	if err := g.Init(); err != nil {
+		return err
+	}
+
 	pdus, err := sms.Encode([]byte(message), sms.To(number), sms.WithAllCharsets)
 	if err != nil {
 		return err
@@ -40,7 +41,7 @@ func (s *MessagingService) sendPDU(number string, message string) error {
 		if err != nil {
 			return err
 		}
-		_, err = s.Modem.SendPDU(tp)
+		_, err = g.SendPDU(tp)
 		if err != nil {
 			return err
 		}
