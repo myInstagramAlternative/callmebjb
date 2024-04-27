3c6cddb2fac4a54c9219779e0addf2b399b7e2a3
myInstagramAlternative <petar@jesteibice.com>
2024-04-27 17:32:33 +0200

    cleanup, go.mod

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.22.2
 
 require (
 	github.com/deepmap/oapi-codegen/v2 v2.1.1-0.20240422103956-472f1cad6201
+	github.com/oapi-codegen/runtime v1.1.1
 	github.com/stretchr/testify v1.9.0
 	github.com/warthog618/modem v0.4.0
 	github.com/warthog618/sms v0.3.0
@@ -11,10 +12,12 @@ require (
 )
 
 require (
+	github.com/apapsch/go-jsonmerge/v2 v2.0.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/getkin/kin-openapi v0.123.0 // indirect
 	github.com/go-openapi/jsonpointer v0.20.2 // indirect
 	github.com/go-openapi/swag v0.22.8 // indirect
+	github.com/google/uuid v1.5.0 // indirect
 	github.com/invopop/yaml v0.2.0 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/mailru/easyjson v0.7.7 // indirect
--- main.go
+++ main.go
@@ -2,83 +2,19 @@ package main
 
 import (
 	"callmebjb/api"
-	// "callmebjb/bond"
 	"callmebjb/utils"
 	"fmt"
-	// "time"
 
 	"log"
 	"net/http"
-	"os"
 	// "github.com/warthog618/sms/encoding/gsm7"
 )
 
-var Config utils.Config
-
-func initConfig() {
-	configPath := os.Getenv("CONFIG_PATH")
-	if configPath == "" {
-		configPath = "config.yaml"
-	}
-	utils.ReadConfig(&Config, configPath)
+func init() {
+	utils.InitConfig()
 }
 
 func main() {
-	initConfig()
-
-	// // Make call
-	// timeout := 15 * time.Second
-	// a, err := bond.InitAt(Config.Modem.Port, Config.Modem.BaudRate, true)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// if err = a.Init(); err != nil {
-	// 	log.Fatal(err)
-	// }
-	// atc := bond.NewATCommander(a, timeout)
-	// if err = atc.Call(""); err != nil {
-	// 	log.Fatal(err)
-	// }
-	// time.Sleep(5 * time.Second)
-
-	// // Hangup
-	// if err = atc.Hangup(); err != nil {
-	// 	log.Fatal(err)
-	// }
-
-	// return
-
-	// // SMS EXAMPLE
-	// modem, err := bond.InitModem(Config.Modem.Port, Config.Modem.BaudRate, false)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-
-	// sender := bond.NewMessagingService(modem, 5*time.Second)
-
-	// if err := sender.Send("", "Test alarm", false); err != nil {
-	// 	log.Fatal(err)
-	// }
-
-	// // USSD EXAMPLE
-	// msg := "*131*1#"
-	// timeout := 15 * time.Second
-	// a, err := bond.InitAt(Config.Modem.Port, Config.Modem.BaudRate, false)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// if err = a.Init(); err != nil {
-	// 	log.Fatal(err)
-	// }
-	// atc := bond.NewATCommander(a, timeout)
-	// res, err := atc.SendUSSD(msg)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// fmt.Printf("%s", res)
-
-	// log.Println("SMS sent successfully!")
-
 	// create a type that satisfies the `api.ServerInterface`, which contains an implementation of every operation from the generated code
 	server := api.NewServer()
 
@@ -87,9 +23,11 @@ func main() {
 	// get an `http.Handler` that we can use
 	h := api.HandlerFromMux(server, r)
 
+	addr := fmt.Sprintf("%s:%s", utils.Config.Server.Listen, utils.Config.Server.Port)
+	log.Printf("Listening on %s", addr)
 	s := &http.Server{
 		Handler: h,
-		Addr:    fmt.Sprintf("%s:%s", Config.Server.Listen, Config.Server.Port),
+		Addr:    addr,
 	}
 
 	// And we serve HTTP until the world ends.
